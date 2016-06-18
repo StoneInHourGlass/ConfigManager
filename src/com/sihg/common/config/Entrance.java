7efc80a468397576a3433daabbad4d670209b499
ShikyohRay <741823774@qq.com>
2016-06-18 18:59:03 +0800

    1st Commit

---
+++ src/com/sihg/common/config/Entrance.java
@@ -0,0 +1,26 @@
+package com.sihg.common.config;
+
+import javax.swing.JFrame;
+
+/**
+ * 配置文件管理器的图形界面入口。
+ * 
+ * @author Ray
+ */
+public class Entrance extends JFrame {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public Entrance() {
+    super();
+    // 关闭的时候 同时结束进程
+    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    setSize(640, 480);
+  }
+
+  public static void main(String[] args) {
+    new Entrance().setVisible(true);
+  }
+}
