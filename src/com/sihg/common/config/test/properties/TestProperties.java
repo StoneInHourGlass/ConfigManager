c50c774f040732f68e186b34912ed4d60ef446e9
ShikyohRay <741823774@qq.com>
2016-06-25 16:38:48 +0800

    ~~~~~~~

--- src/com/sihg/common/config/test/properties/TestProperties.java
+++ src/com/sihg/common/config/test/properties/TestProperties.java
@@ -10,33 +10,31 @@ import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class TestProperties {
-  public static void main(String[] args) {
-<<<<<<< HEAD
-	    System.out.println("Locale.getDefault():[" + Locale.getDefault() + "]");System.out.println("冲突测试");
-	    System.out.println("Locale.JAPANESE:[" + Locale.JAPANESE + "] Locale.JAPAN:[" + Locale.JAPAN + "]");System.out.println("冲突测试");
-	    Locale.setDefault(Locale.JAPAN);System.out.println("冲突测试");
-	    ResourceBundle rb = ResourceBundle.getBundle("text");System.out.println("冲突测试");
-	    System.out.println("edit:[" + rb.getString("edit") + "]");System.out.println("冲突测试");
-	    InputStream in;System.out.println("冲突测试");
-=======
-    System.out.println("Locale.getDefault():[" + Locale.getDefault() + "]");System.out.println("冲突测试");
-    System.out.println("Locale.JAPANESE:[" + Locale.JAPANESE + "] Locale.JAPAN:[" + Locale.JAPAN + "]");System.out.println("冲突测试");
-    Locale.setDefault(Locale.JAPAN);System.out.println("冲突测试");
-    ResourceBundle rb = ResourceBundle.getBundle("text");System.out.println("冲突测试");
-    System.out.println("edit:[" + rb.getString("edit") + "]");System.out.println("冲突测试");
-    InputStream in;System.out.println("冲突测试");
->>>>>>> origin/master
-    try {
-      in = new BufferedInputStream(new FileInputStream("E:/WorkSpaces/GitHub/ConfigManager/test.properties"));
-      Properties p = new Properties();
-      p.load(in);
-      System.out.println();
-      System.out.println();
-      System.out.println("a1:[" + p.getProperty("a1") + "] a2:[" + p.getProperty("a2") + "] a3.b1:[" + p.getProperty("a3.b1") + "]");
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	public static void main(String[] args) {
+		System.out.println("Locale.getDefault():[" + Locale.getDefault() + "]");
+		System.out.println("冲突测试");
+		System.out.println("Locale.JAPANESE:[" + Locale.JAPANESE + "] Locale.JAPAN:[" + Locale.JAPAN + "]");
+		System.out.println("冲突测试");
+		Locale.setDefault(Locale.JAPAN);
+		System.out.println("冲突测试");
+		ResourceBundle rb = ResourceBundle.getBundle("text");
+		System.out.println("冲突测试");
+		System.out.println("edit:[" + rb.getString("edit") + "]");
+		System.out.println("冲突测试");
+		InputStream in;
+		System.out.println("冲突测试");
+		try {
+			in = new BufferedInputStream(new FileInputStream("E:/WorkSpaces/GitHub/ConfigManager/test.properties"));
+			Properties p = new Properties();
+			p.load(in);
+			System.out.println();
+			System.out.println();
+			System.out.println("a1:[" + p.getProperty("a1") + "] a2:[" + p.getProperty("a2") + "] a3.b1:["
+					+ p.getProperty("a3.b1") + "]");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
